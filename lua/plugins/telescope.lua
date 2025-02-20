7024fa3a4633c55d52956daeb50ec2cf7eb92ac3
Joshua <wujoshuawu@gmail.com>
2025-02-21 03:34:14 +0900

    Add devicons to telescope dependencies

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-tree/nvim-web-devicons" },
     {
       "isak102/telescope-git-file-history.nvim",
       dependencies = {
