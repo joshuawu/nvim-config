9d925d72d8f17c01592ebcd6526be9dd4df399e6
Joshua <wujoshuawu@gmail.com>
2025-02-24 12:10:14 +0900

    Add lualine

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,8 @@
-vim.opt.number = true
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
 vim.opt.list = true
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.showmode = false
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.opt.tabstop = 2
