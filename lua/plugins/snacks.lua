0fa2a5c88c5d49c46f62dea1e7d173bf72931732
Joshua <wujoshuawu@gmail.com>
2025-02-25 11:30:55 +0900

    Add snacks.nvim/input

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    input = {
+      win = {
+        relative = "cursor",
+        row = -3,
+        col = 0,
+      },
+    },
+  },
+}
