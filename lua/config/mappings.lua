a9c9b052e28e2929092c65fe1fb6a93afc4fc824
Joshua <wujoshuawu@gmail.com>
2025-02-25 11:24:05 +0900

    Map <C-_> to <C-/>

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -26,6 +26,7 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<C-/>", "gcc", { remap = true })
+vim.keymap.set("n", "<C-_>", "<C-/>", { remap = true })
 vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
 vim.keymap.set("n", "<F7>", "<Cmd>silent! cp | cc<CR>")
 vim.keymap.set("n", "<F8>", "<Cmd>silent! cn | cc<CR>")
@@ -37,3 +38,4 @@ vim.keymap.set("n", "<Leader>q", "<Cmd>" .. mksession_cmd .. " | qall<CR>")
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 vim.keymap.set("t", "jj", "<C-\\><C-n>")
 vim.keymap.set("v", "<C-/>", "gc", { remap = true })
+vim.keymap.set("v", "<C-_>", "<C-/>", { remap = true })
