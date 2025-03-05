295221e39767e56433954b7fd34326d19d2f073a
Joshua <wujoshuawu@gmail.com>
2025-03-06 00:25:05 +0900

    Don't show statusline for diagnostics window

--- lua/plugins/status.lua
+++ lua/plugins/status.lua
@@ -1,6 +1,14 @@
+---@type LazyPluginSpec
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      disabled_filetypes = {
+        statusline = { "trouble" },
+      },
+    },
+  },
   config = function(_, opts)
     require("lualine").setup(opts)
   end,
