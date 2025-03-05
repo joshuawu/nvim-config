6e0be82a29282e9b222e96bf21ee8d49a16fcc06
Joshua <wujoshuawu@gmail.com>
2025-03-05 13:16:21 +0900

    Diagnostics position toggler

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -12,9 +12,9 @@ return {
         win = {
           type = "float",
           relative = "editor",
-          border = "rounded",
           size = { width = 80, height = 10 },
           position = { 0.9, 0.5 },
+          border = "rounded",
           title = "Diagnostics",
           title_pos = "center",
           footer = "<F7> Prev / <F8> Next",
@@ -29,37 +29,89 @@ return {
       },
     },
   },
+  config = function(_, config)
+    require("trouble").setup(config)
+    local prev_opts = {}
+    local prev_win_opts = {}
+    local function toggle_diagnostics_position()
+      local win = require("trouble.view").get({ mode = "diagnostics" })[1].view.win
+      if not win.win then
+        return
+      end
+      local win_opts = vim.api.nvim_win_get_config(win.win)
+      local opts = win.opts
+      if opts.type == "float" then
+        prev_opts.float = vim.fn.copy(opts)
+        prev_win_opts.float = win_opts
+        if prev_opts.split then
+          win.opts = prev_opts.split
+          opts = win.opts
+        else
+          ---@type trouble.Window.split
+          ---@diagnostic disable-next-line: assign-type-mismatch
+          opts = opts
+          opts.type = "split"
+          opts.size = prev_opts.split and prev_opts.split.size or 10
+          opts.position = prev_opts.split and prev_opts.split.position or "bottom"
+          ---@diagnostic disable-next-line: inject-field
+          opts.win = -1
+        end
+        vim.api.nvim_win_set_config(
+          win.win,
+          vim.tbl_extend("force", prev_win_opts.split or {}, {
+            win = -1,
+            relative = "",
+            split = "below",
+            height = 10,
+          })
+        )
+      elseif opts.type == "split" then
+        prev_opts.split = vim.fn.copy(opts)
+        prev_win_opts.split = win_opts
+        if prev_opts.float then
+          win.opts = prev_opts.float
+          opts = win.opts
+        else
+          ---@type trouble.Window.float
+          ---@diagnostic disable-next-line: assign-type-mismatch
+          opts = opts
+          opts.type = "float"
+          opts.size = prev_opts.float and prev_opts.float.size or { width = 80, height = 10 }
+          opts.position = prev_opts.float and prev_opts.float.position or { 0.9, 0.5 }
+        end
+        local y = opts.position[1]
+        local x = opts.position[2]
+        local height = opts.size.height <= 1 and math.floor(vim.o.lines * opts.size.height) or opts.size.height
+        local width = opts.size.width <= 1 and math.floor(vim.o.columns * opts.size.width) or opts.size.width
+        local row = math.abs(y) <= 1 and math.floor((vim.o.lines - height) * y) or y
+        row = row < 0 and (vim.o.lines + row) or row
+        local col = math.abs(x) <= 1 and math.floor((vim.o.columns - width) * x) or x
+        col = col < 0 and (vim.o.columns + col) or col
+        vim.api.nvim_win_set_config(
+          win.win,
+          vim.tbl_extend("force", prev_win_opts.float or {}, {
+            relative = "editor",
+            row = row,
+            col = col,
+            width = width,
+            height = height,
+          })
+        )
+      end
+    end
+    vim.keymap.set("n", "<Leader>x", toggle_diagnostics_position, { desc = "Toggle diagnostics position" })
+  end,
   cmd = "Trouble",
   keys = {
     {
-      "<leader>xx",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xX",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
-    },
-    {
       "<leader>cs",
       "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "Symbols (Trouble)",
+      desc = "Toggle symbols explorer",
     },
     {
       "<leader>cl",
       "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
-    },
-    {
-      "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
-    },
-    {
-      "<leader>xQ",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
+      desc = "Toggle LSP info explorer",
     },
     {
       "<F7>",
@@ -79,5 +131,25 @@ return {
       end,
       desc = "Go to next error",
     },
+    -- {
+    --   "<leader>xx",
+    --   "<cmd>Trouble diagnostics toggle<cr>",
+    --   desc = "Diagnostics (Trouble)",
+    -- },
+    -- {
+    --   "<leader>xX",
+    --   "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+    --   desc = "Buffer Diagnostics (Trouble)",
+    -- },
+    -- {
+    --   "<leader>xL",
+    --   "<cmd>Trouble loclist toggle<cr>",
+    --   desc = "Location List (Trouble)",
+    -- },
+    -- {
+    --   "<leader>xQ",
+    --   "<cmd>Trouble qflist toggle<cr>",
+    --   desc = "Quickfix List (Trouble)",
+    -- },
   },
 }
